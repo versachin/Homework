33395fdb245ea4d0eed36690d0665227d18324d1
Sachin Palta <sachinpalta@gmail.com>
2018-02-02 11:16:46 -0500

    gheiqtjbnqoejbpoejb

--- Chapter - 8/Chapter 8 Doc.py
+++ Chapter - 8/Chapter 8 Doc.py
@@ -7,6 +7,22 @@ for letter in prefixes:
    else:
        print(letter+suffix)
 
+#Problem 3
+def count_letters(word,letter):
+    count = 0
+    for char in word:
+        if char == letter
+            count += 1
+    return count
+
+count_letters("soup","o")
+
+#Problem 7
+def reverse(word):
+    for char in word:
+        new_word = word[len(word)] + word[len(word)-1] + word[len(word)-2] +
+
+
 
 
 
