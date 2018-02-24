a1f2048e10d77a5ad8043cea73c38c05901446aa
Sachin Palta <sachinpalta@gmail.com>
2018-02-24 18:19:40 -0500

    did a little bit

---
+++ Chapter - 8/pygame practice/Pygame Shapes Practice.py
@@ -0,0 +1,13 @@
+import pygame
+
+pygame.init()
+
+white = (255,255,255)
+black = (0,0,0)
+
+red = (255,0,0)
+green = (0,255,0)
+blue = (0,0,255)
+
+gameDisplay = pygame.display.set_mode((800,600))
+gameDisplay.fill(black)
