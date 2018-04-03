954df352df96eb4aacdb9834e168933be015ec8d
Sachin Palta <sachinpalta@gmail.com>
2018-04-02 21:16:24 -0400

    did 2 and 3

--- Chapter 9 Excercise 1.py
+++ Chapter 9 Excercise 1.py
@@ -9,3 +9,8 @@ def f(ar1):
 f(tuplist)
 
 #YES it is possible
+#Is a pair a generalization of a tuple, or is a tuple a generalization of a pair?
+#A tuple is a generlization of a pair
+
+# Is a pair a kind of tuple, or is a tuple a kind of pair?
+#A pair is a kind of tuple
